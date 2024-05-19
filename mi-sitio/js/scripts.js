d3447f6dfbfa32fea806536adfff49ebfbe66044
FabiDevXd <enturionfabian@gmail.com>
2024-05-18 22:23:55 -0400

    Add files via upload

---
+++ mi-sitio/js/scripts.js
@@ -0,0 +1,5 @@
+// js/scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página completamente cargada');
+    // Puedes agregar más funciones de JavaScript aquí
+});
